4d7f77e2ccbf81d08d1cd36cad78d68daddcdd39
renickmaina <147622708+renickmaina@users.noreply.github.com>
2025-09-01 11:27:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ MYSQL_DB = "moodjournal"
 
 # Hugging Face API
 HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment"
-HUGGINGFACE_API_KEY = "your_huggingface_token"
+HUGGINGFACE_API_KEY = "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"
 
 # IntaSend Test Key (replace with live key later)
-INTASEND_PUBLISHABLE_KEY = "test_publishable_key"
+INTASEND_PUBLISHABLE_KEY = "ISPubKey_test_772dee5d-452b-4720-835d-53e0c3e9538e"
+
